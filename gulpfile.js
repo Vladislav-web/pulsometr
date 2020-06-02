743c4673eb1f95a1f31ff1a22b7b9db62f3ae715
Vladislav-web <ykt.pc_master@mail.ru>
2020-06-02 10:59:30 +0300

    added benefit

--- gulpfile.js
+++ gulpfile.js
@@ -7,45 +7,45 @@ const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function () {
-  browserSync.init({
-    server: {
-      baseDir: "src"
-    },
-    notify: {
-      styles: [
-        'display: block; ',
-        'padding: 6px 15px 3px;',
-        'position: fixed;',
-        'font-size: 10px;',
-        'z-index: 9999;',
-        'right: 0px;',
-        'top: 0px;',
-        'color: rgb(74, 74, 74);',
-      ]
-    }
-  });
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        },
+        notify: {
+            styles: [
+                'display: block; ',
+                'padding: 6px 15px 3px;',
+                'position: fixed;',
+                'font-size: 14px;',
+                'z-index: 9999;',
+                'right: 0px;',
+                'top: 0px;',
+                'color: rgb(255, 0, 0);',
+            ]
+        }
+    });
 });
 
 gulp.task('styles', function () {
-  return gulp.src("src/sass/**/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
     .pipe(sass({
-      outputStyle: 'compressed'
+        outputStyle: 'compressed'
     }).on('error', sass.logError))
     .pipe(rename({
-      prefix: "",
-      suffix: ".min",
+        prefix: "",
+        suffix: ".min",
     }))
     .pipe(autoprefixer())
     .pipe(cleanCSS({
-      compatibility: 'ie8'
+        compatibility: 'ie8'
     }))
     .pipe(gulp.dest("src/css"))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream())
 })
 
 gulp.task('watch', function () {
-  gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel("styles"))
-  gulp.watch('src/*.html').on('change', browserSync.reload);
+    gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel("styles"))
+    gulp.watch('src/*.html').on('change', browserSync.reload);
 })
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
