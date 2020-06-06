fb60ae2e5e8c8925507cc7975aade217c58f36a3
Vladislav-web <ykt.pc_master@mail.ru>
2020-06-06 18:19:22 +0300

    final project

--- src/js/main.js
+++ src/js/main.js
@@ -25,24 +25,6 @@ $(document).ready(function () {
             .closest('div.main__container').find('div.catalog__content').removeClass('catalog__content--active').eq($(this).index()).addClass('catalog__content--active');
     });
 
-    // вход в карточку
-    // $('.catalog-item__link').each(function (i) {
-    //   $(this).on('click', function (e) {
-    //       e.preventDefault();
-    //       $('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
-    //       $('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
-    //   })
-    // })
-
-    // выход из карточки
-    // $('.catalog-item__back').each(function (i) {
-    //   $(this).on('click', function (e) {
-    //       e.preventDefault();
-    //       $('.catalog-item__content').eq(i).toggleClass('catalog-item__content--active');
-    //       $('.catalog-item__list').eq(i).toggleClass('catalog-item__list--active');
-    //   })
-    // })
-
     //! оптимизированный вариант
     function toggleSlide(item) {
         $(item).each(function (i) {
